140d68fb1ba2eab414089be0a8e461924fae5697
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-02-26 22:41:42 -0500

    moved variables into function so page doesnt need to be refreshed for new password to work correctly

--- Develop/script.js
+++ Develop/script.js
@@ -13,14 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// array of possible characters
-var lowerCaseLetter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-var upperCaseLetter = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
-var specialChar = [" ", "!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "]", "[", "^", "_", "`", "{", "|", "}", "~"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var pwChar = [];
-
 // Prompt for number of characters in password
 function generatePassword(){
   var pwLength = parseInt(prompt("Please choose how many characters you would like your password to be. (Between 8-128 characters)"));
@@ -30,6 +22,14 @@ function generatePassword(){
     alert("Password needs to be between 8 to 128 characters long");
     return;
   }
+  
+  // array of possible characters
+  var lowerCaseLetter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+  var upperCaseLetter = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+  var specialChar = [" ", "!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "]", "[", "^", "_", "`", "{", "|", "}", "~"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var pwChar = [];
+
   // Prompts for choosing what kind of characters needed in password
   var needLowerCase = confirm("Would you like lowercase characters in your password? (Ok for yes or Cancel for no)");
   var needUpperCase = confirm("Would you like uppercase characters in your password? (Ok for yes or Cancel for no)");
@@ -61,9 +61,12 @@ function generatePassword(){
   // takes the choices and generates the password
   for (var i = 0; i < pwLength; i++){
     newPassword = newPassword + pwChar[Math.floor(Math.random() * pwChar.length)];
-    console.log(newPassword)
+    console.log(newPassword);
   }
 
+  // shows password in a pop up notification
+  alert("Your password is " + newPassword);
+  
   // shows the generated password in the text box
   return newPassword;
 }
