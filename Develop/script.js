59e937e0057720f56d233950dd9a533f26bbb073
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-02-26 22:32:53 -0500

    added randomizer and display pw

--- Develop/script.js
+++ Develop/script.js
@@ -30,13 +30,40 @@ function generatePassword(){
     alert("Password needs to be between 8 to 128 characters long");
     return;
   }
+  // Prompts for choosing what kind of characters needed in password
   var needLowerCase = confirm("Would you like lowercase characters in your password? (Ok for yes or Cancel for no)");
   var needUpperCase = confirm("Would you like uppercase characters in your password? (Ok for yes or Cancel for no)");
   var needSpecialChar = confirm("Would you like special characters in your password? (Ok for yes or Cancel for no)");
   var needNumber = confirm("Would you like number characters in your password? (Ok for yes or Cancel for no)");
 
+  // Making sure at least one option is chosen
   if (needLowerCase === false && needUpperCase === false && needSpecialChar === false && needNumber === false){
     alert("Password needs at least one of the options.");
     return;
   }
+  // adding characters to the password if needed from promnpts
+  if (needLowerCase) {
+    pwChar = pwChar.concat(lowerCaseLetter);
+  }
+  if (needUpperCase) {
+    pwChar = pwChar.concat(upperCaseLetter);
+  }
+  if (needSpecialChar) {
+    pwChar = pwChar.concat(specialChar);
+  }
+  if (needNumber) {
+    pwChar = pwChar.concat(numbers);
+  }
+
+  // storing generated password
+  var newPassword = "";
+
+  // takes the choices and generates the password
+  for (var i = 0; i < pwLength; i++){
+    newPassword = newPassword + pwChar[Math.floor(Math.random() * pwChar.length)];
+    console.log(newPassword)
+  }
+
+  // shows the generated password in the text box
+  return newPassword;
 }
